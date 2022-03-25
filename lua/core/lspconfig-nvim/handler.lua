afbaed7ea5613d987022c831ee6e3dfb48b3a830
RyaWcksn <pram.aryawcksn@protonmail.ch>
2022-03-25 15:45:26 +0700

    Update

---
+++ lua/core/lspconfig-nvim/handler.lua
@@ -0,0 +1,52 @@
+local M = {}
+
+M.setup = function ()
+    local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+    for type, icon in pairs(signs) do
+        local hl = "DiagnosticSign" .. type
+        vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+    end
+
+    local config = {
+        -- disable virtual text
+        virtual_text = false,
+        -- show signs
+        signs = {
+            active = signs,
+        },
+        update_in_insert = true,
+        underline = true,
+        severity_sort = true,
+        float = {
+            focusable = false,
+            style = "minimal",
+            border = "rounded",
+            source = "always",
+            header = "",
+            prefix = "",
+        },
+    }
+    vim.diagnostic.config(config)
+    vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+      border = "rounded",
+    })
+
+    vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+      border = "rounded",
+    })
+
+    vim.o.updatetime = 250
+    vim.cmd([[autocmd! CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]])
+    vim.cmd([[autocmd! CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false, scope="cursor"})]])
+end
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+
+local status_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
+if not status_ok then
+    return
+end
+
+M.capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
+
+return M
---
+++ lua/core/lspconfig-nvim/lsp_installer.lua
@@ -0,0 +1,45 @@
+local no_err, lsp_installer = pcall(require,"nvim-lsp-installer")
+if not no_err then
+    return
+end
+
+lsp_installer.on_server_ready(function(server)
+    local opts = {
+		on_attach = require("core.lspconfig-nvim.handler").on_attach,
+		capabilities = require("core.lspconfig-nvim.handler").capabilities,
+	}
+	local lsp_installer_servers = require("nvim-lsp-installer.servers")
+
+	local servers = {
+		"bashls",
+		"vuels",
+		"cland",
+		"cssls",
+		"html",
+		"jsonls",
+		"sumneko_lua",
+		"phpactor",
+		"pyright",
+		"solargraph",
+		"tsserver",
+        "gopls",
+	}
+
+	for _, name in pairs(servers) do
+		---@diagnostic disable-next-line: redefined-local
+		local ok, server = lsp_installer_servers.get_server(name)
+		if ok then
+			if not server:is_installed() then
+				server:install()
+			end
+		end
+
+        if name == "sumneko_lua" then
+            local sumneko_opts = require("core.lspconfig-nvim.setting.sumneko_lua")
+            opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
+        end
+	end
+
+	server:setup(opts)
+	vim.cmd([[ do User LspAttachBuffers ]])
+end)
