68ce16b732d4719e80b6c0c36c8b45481555f6a8
Pramudya Arya Wicaksana <pramudyaarya@ayoconnect.id>
2022-05-13 08:56:00 +0700

    Update

--- lua/core/nvimtree-nvim/init.lua
+++ lua/core/nvimtree-nvim/init.lua
@@ -9,7 +9,10 @@ local g = vim.g
 vim.o.termguicolors = true
 g.nvim_tree_add_trailing = 0
 g.nvim_tree_show_icons = {
-    folder_arrow = 0
+   folders = 1,
+   files = 1,
+   git = 1,
+   folder_arrows = 0,
 }
 g.nvim_tree_icons = {
     default = "",
@@ -50,7 +53,6 @@ nvimtree.setup {
     hijack_netrw                       = true,
     open_on_setup                      = false,
     ignore_ft_on_setup                 = {},
-    -- auto_close           = false,
     auto_reload_on_write               = true,
     open_on_tab                        = false,
     hijack_cursor                      = false,
@@ -62,7 +64,31 @@ nvimtree.setup {
     },
     view                               = {
         side = "left",
+        hide_root_folder = false,
+        preserve_window_proportions = false,
         width = 25,
+        mappings = {
+            custom_only = false,
+            list = {
+                { key = "l", action = "edit", action_cb = edit_or_open },
+                { key = "L", action = "vsplit_preview", action_cb = vsplit_preview },
+                { key = "h", action = "close_node" },
+                { key = "H", action = "collapse_all", action_cb = collapse_all }
+            }
+        },
+    },
+    renderer                           = {
+        indent_markers = {
+            enable = false,
+            icons = {
+                corner = "└ ",
+                edge = "│ ",
+                none = "  ",
+            },
+        },
+        icons = {
+            webdev_colors = true,
+        },
     },
     filters                            = {
         dotfiles = false,
@@ -72,5 +98,29 @@ nvimtree.setup {
         enable = true,
         ignore = true,
         timeout = 500,
+    },
+    actions                            = {
+        use_system_clipboard = true,
+        change_dir = {
+            enable = true,
+            global = false,
+            restrict_above_cwd = false,
+        },
+        open_file = {
+            quit_on_open = false,
+            resize_window = false,
+            window_picker = {
+                enable = true,
+                chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
+                exclude = {
+                    filetype = { "notify", "packer", "qf", "diff", "fugitive", "fugitiveblame" },
+                    buftype = { "nofile", "terminal", "help" },
+                },
+            },
+        },
+    },
+    trash                              = {
+        cmd = "trash",
+        require_confirm = true,
     },
 }
