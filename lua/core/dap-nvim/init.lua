e18140f2951344b04c8d590aac8e39529d869187
Pramudya Arya Wicaksana <pramudyaarya@ayoconnect.id>
2022-08-18 17:52:38 +0700

    update

--- lua/core/dap-nvim/init.lua
+++ lua/core/dap-nvim/init.lua
@@ -1,53 +1,39 @@
-local dap = require('dap')
-
-require("nvim-dap-virtual-text").setup {
-    commented = true,
-}
-require('dap-go').setup()
-
-dap.adapters.go = function(callback, config)
-    local stdout = vim.loop.new_pipe(false)
-    local handle
-    local pid_or_err
-    local port = 38697
-    local opts = {
-        stdio = { nil, stdout },
-        args = { "dap", "-l", "127.0.0.1:" .. port },
-        detached = true
-    }
-    handle, pid_or_err = vim.loop.spawn("dlv", opts, function(code)
-        stdout:close()
-        handle:close()
-        if code ~= 0 then
-            print('dlv exited with code', code)
-        end
-    end)
-    assert(handle, 'Error running dlv: ' .. tostring(pid_or_err))
-    stdout:read_start(function(err, chunk)
-        assert(not err, err)
-        if chunk then
-            vim.schedule(function()
-                require('dap.repl').append(chunk)
-            end)
-        end
-    end)
-    -- Wait for delve to start
-    vim.defer_fn(
-        function()
-            callback({ type = "server", host = "127.0.0.1", port = port })
-        end,
-        100)
+local dap, dapui = require("dap"), require("dapui")
+dap.listeners.after.event_initialized["dapui_config"] = function()
+    dapui.open()
+end
+dap.listeners.before.event_terminated["dapui_config"] = function()
+    dapui.close()
+end
+dap.listeners.before.event_exited["dapui_config"] = function()
+    dapui.close()
 end
 
+
+require("nvim-dap-virtual-text").setup()
+require('dap-go').setup()
+require("dapui").setup()
+
+
+dap.adapters.delve = {
+    type = 'server',
+    port = '${port}',
+    executable = {
+        command = 'dlv',
+        args = { 'dap', '-l', '127.0.0.1:${port}' },
+    }
+}
+
+-- https://github.com/go-delve/delve/blob/master/Documentation/usage/dlv_dap.md
 dap.configurations.go = {
     {
-        type = "go",
+        type = "delve",
         name = "Debug",
         request = "launch",
         program = "${file}"
     },
     {
-        type = "go",
+        type = "delve",
         name = "Debug test", -- configuration for debugging test files
         request = "launch",
         mode = "test",
@@ -55,77 +41,17 @@ dap.configurations.go = {
     },
     -- works with go.mod packages and sub packages
     {
-        type = "go",
+        type = "delve",
         name = "Debug test (go.mod)",
         request = "launch",
         mode = "test",
         program = "./${relativeFileDirname}"
     }
 }
-dap.configurations.lua = {
-    {
-      type = "nlua",
-      request = "attach",
-      name = "Attach to running Neovim instance",
-      host = function()
-        local value = vim.fn.input "Host [127.0.0.1]: "
-        if value ~= "" then
-          return value
-        end
-        return "127.0.0.1"
-      end,
-      port = function()
-        local val = tonumber(vim.fn.input("Port: ", "54321"))
-        assert(val, "Please provide a port number")
-        return val
-      end,
-    },
-  }
 
-  dap.adapters.nlua = function(callback, config)
-    callback { type = "server", host = config.host, port = config.port }
-  end
 
-require("dapui").setup({
-    icons = { expanded = "▾", collapsed = "▸" },
-    mappings = {
-        -- Use a table to apply multiple mappings
-        expand = { "<CR>", "<2-LeftMouse>" },
-        open = "o",
-        remove = "d",
-        edit = "e",
-        repl = "r",
-    },
-    layouts = {
-        {
-            elements = {
-                'scopes',
-                'breakpoints',
-                'stacks',
-                'watches',
-            },
-            size = 40,
-            position = 'left',
-        },
-        {
-            elements = {
-                'repl',
-                'console',
-            },
-            size = 10,
-            position = 'bottom',
-        },
-    },
-    floating = {
-        max_height = nil, -- These can be integers or a float between 0 and 1.
-        max_width = nil, -- Floats will be treated as percentage of your screen.
-        border = "single", -- Border style. Can be "single", "double" or "rounded"
-        mappings = {
-            close = { "q", "<Esc>" },
-        },
-    },
-    windows = { indent = 1 },
-})
+
+
 local dap_breakpoint = {
     error = {
         text = "🟥",
