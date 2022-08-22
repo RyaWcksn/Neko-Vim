261599071378673131206268cdfca1daa843143a
RyaWcksn <pram.aryawcksn@protonmail.ch>
2022-08-22 05:25:25 +0000

    update

--- lua/core/treesitter-nvim/init.lua
+++ lua/core/treesitter-nvim/init.lua
@@ -1,20 +1,70 @@
 require("nvim-treesitter.configs").setup({
-	require('utils.nekorc').languages.ensure_installed,
-	sync_install = true,
-	highlight = {
-		enable = true,
-		additional_vim_regex_highlighting = false,
-	},
+    require('utils.nekorc').languages.ensure_installed,
+    sync_install = true,
+    highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+    },
+    context_commentstring = {
+        enable = true,
+        enable_autocmd = false,
+        config = {
+            -- Languages that have a single comment style
+            typescript = "// %s",
+            css = "/* %s */",
+            scss = "/* %s */",
+            html = "<!-- %s -->",
+            svelte = "<!-- %s -->",
+            vue = "<!-- %s -->",
+            json = "",
+        },
+    },
+    indent = { enable = true },
+    autotag = { enable = false },
+    textobjects = {
+        swap = {
+            enable = false,
+            -- swap_next = textobj_swap_keymaps,
+        },
+        -- move = textobj_move_keymaps,
+        select = {
+            enable = false,
+            -- keymaps = textobj_sel_keymaps,
+        },
+    },
+    textsubjects = {
+        enable = false,
+        keymaps = { ["."] = "textsubjects-smart", [";"] = "textsubjects-big" },
+    },
+    playground = {
+      enable = false,
+      disable = {},
+      updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+      persist_queries = false, -- Whether the query persists across vim sessions
+      keybindings = {
+        toggle_query_editor = "o",
+        toggle_hl_groups = "i",
+        toggle_injected_languages = "t",
+        toggle_anonymous_nodes = "a",
+        toggle_language_display = "I",
+        focus_language = "f",
+        unfocus_language = "F",
+        update = "R",
+        goto_node = "<cr>",
+        show_help = "?",
+      },
+    },
+    rainbow = {
+      enable = false,
+      extended_mode = true, -- Highlight also non-parentheses delimiters, boolean or table: lang -> boolean
+      max_file_lines = 1000, -- Do not enable for files with more than 1000 lines, int
+    },
 })
 
-require'treesitter-context'.setup{
+require 'treesitter-context'.setup {
     enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
     max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
     patterns = { -- Match patterns for TS nodes. These get wrapped to match at word boundaries.
-        -- For all filetypes
-        -- Note that setting an entry here replaces all other patterns for this entry.
-        -- By setting the 'default' entry below, you can control which nodes you want to
-        -- appear in the context window.
         default = {
             'class',
             'function',
@@ -32,14 +82,8 @@ require'treesitter-context'.setup{
         --   },
     },
     exact_patterns = {
-        -- Example for a specific filetype with Lua patterns
-        -- Treat patterns.rust as a Lua pattern (i.e "^impl_item$" will
-        -- exactly match "impl_item" only)
         -- rust = true,
     },
-
-    -- [!] The options below are exposed but shouldn't require your attention,
-    --     you can safely ignore them.
 
     zindex = 20, -- The Z-index of the context window
 }
