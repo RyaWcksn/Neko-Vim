f6f0567e1acd7ae1caeb8c83ed4933e6b117912a
Pramudya Arya Wicaksana <pramudyaarya@ayoconnect.id>
2022-11-14 15:01:41 +0700

    fix: UPDATE

--- lua/core/dap-nvim/init.lua
+++ lua/core/dap-nvim/init.lua
@@ -1,5 +1,41 @@
-local dap, dapui = require("dap"), require("dapui")
+-- vim.keymap.set("n", "<F5>", ":lua require'dap'.continue()<CR>")
+vim.keymap.set("n", "<F3>", ":lua require'dap'.step_over()<CR>")
+vim.keymap.set("n", "<F2>", ":lua require'dap'.step_into()<CR>")
+vim.keymap.set("n", "<F12>", ":lua require'dap'.step_out()<CR>")
+vim.keymap.set("n", "<leader>b", ":lua require'dap'.toggle_breakpoint()<CR>")
+vim.keymap.set("n", "<leader>B", ":lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>")
+vim.keymap.set("n", "<leader>lp", ":lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<CR>")
+vim.keymap.set("n", "<leader>dr", ":lua require'dap'.repl.open()<CR>")
+vim.keymap.set("n", "<leader>dt", ":lua require'dap-go'.debug_test()<CR>")
 
+--[[local dap_breakpoint = {
+    error = {
+        text = "🟥",
+        texthl = "LspDiagnosticsSignError",
+        linehl = "",
+        numhl = "",
+    },
+    rejected = {
+        text = "",
+        texthl = "LspDiagnosticsSignHint",
+        linehl = "",
+        numhl = "",
+    },
+    stopped = {
+        text = "⭐️",
+        texthl = "LspDiagnosticsSignInformation",
+        linehl = "DiagnosticUnderlineInfo",
+        numhl = "LspDiagnosticsSignInformation",
+    },
+}
+vim.fn.sign_define("DapBreakpoint", dap_breakpoint.error)
+vim.fn.sign_define("DapStopped", dap_breakpoint.stopped)
+vim.fn.sign_define("DapBreakpointRejected", dap_breakpoint.rejected)
+require("nvim-dap-virtual-text").setup()
+require('dap-go').setup()
+require("dapui").setup()
+
+local dap, dapui = require("dap"), require("dapui")
 dap.listeners.after.event_initialized["dapui_config"] = function()
     dapui.open()
 end
@@ -10,22 +46,11 @@ dap.listeners.before.event_exited["dapui_config"] = function()
     dapui.close()
 end
 
-
-require("nvim-dap-virtual-text").setup()
-require('dap-go').setup()
-require("dapui").setup()
-
-
 dap.adapters.delve = {
-    type = 'server',
-    port = '${port}',
-    executable = {
-        command = 'dlv',
-        args = { 'dap', '-l', '127.0.0.1:${port}' },
-    }
+    type = "server",
+    host = "127.0.0.1",
+    port = 38697,
 }
-
--- https://github.com/go-delve/delve/blob/master/Documentation/usage/dlv_dap.md
 dap.configurations.go = {
     {
         type = "delve",
@@ -49,31 +74,70 @@ dap.configurations.go = {
         program = "./${relativeFileDirname}"
     }
 }
+ ]]
+local M = {}
 
+local function configure()
+  local dap_install = require "dap-install"
+  dap_install.setup {
+    installation_path = vim.fn.stdpath "data" .. "/dapinstall/",
+  }
 
-
-
-local dap_breakpoint = {
+  local dap_breakpoint = {
     error = {
-        text = "🟥",
-        texthl = "LspDiagnosticsSignError",
-        linehl = "",
-        numhl = "",
+      text = "🟥",
+      texthl = "LspDiagnosticsSignError",
+      linehl = "",
+      numhl = "",
     },
     rejected = {
-        text = " ",
-        texthl = "LspDiagnosticsSignHint",
-        linehl = "",
-        numhl = "",
+      text = "",
+      texthl = "LspDiagnosticsSignHint",
+      linehl = "",
+      numhl = "",
     },
     stopped = {
-        text = "⭐️",
-        texthl = "LspDiagnosticsSignInformation",
-        linehl = "DiagnosticUnderlineInfo",
-        numhl = "LspDiagnosticsSignInformation",
+      text = "⭐️",
+      texthl = "LspDiagnosticsSignInformation",
+      linehl = "DiagnosticUnderlineInfo",
+      numhl = "LspDiagnosticsSignInformation",
     },
-}
+  }
 
-vim.fn.sign_define("DapBreakpoint", dap_breakpoint.error)
-vim.fn.sign_define("DapStopped", dap_breakpoint.stopped)
-vim.fn.sign_define("DapBreakpointRejected", dap_breakpoint.rejected)
+  vim.fn.sign_define("DapBreakpoint", dap_breakpoint.error)
+  vim.fn.sign_define("DapStopped", dap_breakpoint.stopped)
+  vim.fn.sign_define("DapBreakpointRejected", dap_breakpoint.rejected)
+end
+
+local function configure_exts()
+  require("nvim-dap-virtual-text").setup {
+    commented = true,
+  }
+
+  local dap, dapui = require "dap", require "dapui"
+  dapui.setup {} -- use default
+  dap.listeners.after.event_initialized["dapui_config"] = function()
+    dapui.open()
+  end
+  dap.listeners.before.event_terminated["dapui_config"] = function()
+    dapui.close()
+  end
+  dap.listeners.before.event_exited["dapui_config"] = function()
+    dapui.close()
+  end
+end
+
+local function configure_debuggers()
+  require("core.dap-nvim.lang.go").setup()
+end
+
+function M.setup()
+  configure() -- Configuration
+  configure_exts() -- Extensions
+  configure_debuggers() -- Debugger
+  require("config.dap.keymaps").setup() -- Keymaps
+end
+
+configure_debuggers()
+
+return M
